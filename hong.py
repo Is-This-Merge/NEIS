03e4ee4f44858e2511079fcab657141277cc0413
AcE3742-SASA <ejunhong03@sasa.hs.kr>
2026-05-27 12:31:04 +0900

    Update hong.py

--- hong.py
+++ hong.py
@@ -16,3 +16,16 @@ class Player():
                 self.ball.runaway()
     def replaceFlamingo(self):
         self.currentFlamingo += 1
+
+class GamePlayer(Player):
+    def __init__(self):
+        super().__init__()
+        self.availableSoldiers = []
+    def soldierArrange(self):
+        if self.availableSoldiers == []:
+            print("배치 가능한 병사가 없습니다.")
+        else:
+            print("배치 가능한 병사 리스트:\n", self.availableSoldiers)
+            curSoldier = int(input("병사의 index를 입력하세요(1부터 시작): "))
+            toGoal = int(input("목적지 골대번호를 입력하세요: "))
+            self.availableSoldiers[curSoldier].move(toGoal)

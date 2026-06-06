7c2d3c3f4b94278dcd69697c53a004a64a510663
AcE3742-SASA <ejunhong03@sasa.hs.kr>
2026-06-06 22:50:46 +0900

    edit Soldier.execute_queen_command

--- Soldier.py
+++ Soldier.py
@@ -1,19 +1,17 @@
 from Object import Object
 
 class Soldier(Object):
-    def __init__(self, cooldown=0, usable=True, assignedGoal=None, match=None):
+    def __init__(self, cooldown=2, usable=True, assignedGoal=None, match=None):
         super().__init__(usable)
         self.cooldown = cooldown
         self.assignedGoal = assignedGoal
         self.match = match
+        self.leftTurn = 0
 
     def execute_queen_command(self):
-        if self.usable:
-            print(f"카드 병사가 여왕의 명령을 수행합니다! (복귀까지 남은 시간: {self.cooldown}초)")
-            if self.assignedGoal is not None:
-                print(f" -> 현재 소속: {self.assignedGoal.order}번 골대")
-        else:
-            print("현재 카드 병사를 사용할 수 없는 상태(usable=False)입니다.")
+        print(f"카드 병사가 여왕의 명령을 수행합니다! (복귀까지 남은 시간: {self.cooldown}턴)")
+        self.leftTurn = self.match.currentTurn
+        self.assignedGoal = None
 
     def move(self, destination):
         if not self.usable:
